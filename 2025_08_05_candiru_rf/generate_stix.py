b8dc973fc18628937552cd8e98cd85b9e018a2c7
Jurre van Bergen <jurre.vanbergen@amnesty.org>
2025-08-07 13:45:23 +0100

    commit rf candiru domains from their report

---
+++ 2025_08_05_candiru_rf/generate_stix.py
@@ -0,0 +1,32 @@
+import sys
+import os
+from stix2.v21 import (Indicator, Malware, Relationship, Bundle, DomainName)
+
+
+if __name__ == "__main__":
+    malware_name = "Candiru"
+    stix_name = "candiru.stix2"
+    if os.path.isfile(stix_name):
+        os.remove(stix_name)
+
+    with open("domains.txt") as f:
+        domains = list(set([a.strip() for a in f.read().split()]))
+
+    with open("processes.txt") as f:
+        processes = list(set([a.strip() for a in f.read().split()]))
+
+    with open("emails.txt") as f:
+        emails = list(set([a.strip() for a in f.read().split()]))
+
+    res = []
+    malware = Malware(name=malware_name, is_family=False, description="IOCs related to Candiru's DevilsTongue as documented by Recorded Future.")
+    res.append(malware)
+    for d in domains:
+        i = Indicator(indicator_types=["malicious-activity"], pattern="[domain-name:value='{}']".format(d), pattern_type="stix")
+        res.append(i)
+        res.append(Relationship(i, 'indicates', malware))
+
+    bundle = Bundle(objects=res)
+    with open(stix_name, "w+") as f:
+        f.write(bundle.serialize(indent=4))
+    print("{} file created".format(stix_name))
